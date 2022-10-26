1f4508f5a2401f0594232c19927f4061df2e1433
mdayann <mdayan312@gmail.com>
2022-10-26 23:32:21 +0700

    feat: add weather module

---
+++ discord.js
@@ -0,0 +1,62 @@
+const {
+  Client,
+  Events,
+  GatewayIntentBits,
+  ActivityType,
+} = require('discord.js');
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers,
+  ],
+});
+const cron = require('cron');
+
+require('dotenv').config();
+const weather = require('./weather');
+
+const log4js = require('log4js');
+
+const logger = log4js.getLogger();
+logger.level = 'debug';
+
+function mainDiscord() {
+  try {
+    client.login(process.env.DISCORD_TOKEN);
+
+    client.on('ready', () => {
+      logger.debug(`Logged in as ${client.user.tag}!`);
+      client.user.setActivity('Pentil', { type: ActivityType.Streaming });
+
+
+      let scheduledMessage = new cron.CronJob('*/10 * * * * *', async () => {
+           const guild = client.guilds.cache.get('312965119250399234');
+           const channel = guild.channels.cache.get('312965119250399234');
+
+           let weatherMessage = await weather()
+
+           channel.send(weatherMessage);
+          });
+
+          scheduledMessage.start()
+
+    });
+
+    client.on('interactionCreate', async (interaction) => {
+      if (!interaction.isChatInputCommand()) return;
+
+      if (interaction.commandName === 'jokopentil') {
+        const pdip = client.emojis.cache.find((emoji) => emoji.name === 'pdip');
+        await interaction.reply(`Received request : ${pdip}`);
+        logger.debug(`Received command request : ${interaction.commandName}`);
+      }
+    });
+
+  } catch (error) {
+    logger.error(`Error Happened : ${error}`);
+  }
+}
+
+module.exports = mainDiscord;
---
+++ weather.js
@@ -0,0 +1,35 @@
+require('dotenv').config();
+const fetch = require('node-fetch');
+
+const log4js = require('log4js');
+
+const logger = log4js.getLogger();
+logger.level = 'debug';
+
+async function mainWeather() {
+  try {
+    const response = await fetch(`http://dataservice.accuweather.com/forecasts/v1/hourly/12hour/211671?apikey=${process.env.ACCU_API_KEY}&language=id-id`)
+    const json = await response.json()
+    if (response.ok) {
+
+        let morningStatus = json[0].IconPhrase
+        let eveningStatus = json[6].IconPhrase
+        let nightStatus = json[11].IconPhrase
+
+        let morningDegree = parseFloat((json[0].Temperature.Value - 32) / 1.8).toFixed(1)
+        let eveningDegree = parseFloat((json[6].Temperature.Value - 32) / 1.8).toFixed(1)
+        let nightDegree = parseFloat((json[11].Temperature.Value - 32) / 1.8).toFixed(1)
+
+        const weatherMessage = `Ramalan cuaca Yogyakarta hari ini : \nPagi : ${morningStatus} ${morningDegree}°C \nSiang : ${eveningStatus} ${eveningDegree}°C \nPagi : ${nightStatus} ${nightDegree}°C`
+
+        console.log(weatherMessage)
+        return weatherMessage
+    } else {
+        logger.error(`Error Happened : ${error}`);
+    }
+  } catch (error) {
+    logger.error(`Error Happened : ${error}`);
+  }
+}
+
+module.exports=mainWeather
